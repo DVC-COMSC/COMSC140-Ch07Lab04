dea7c6f54b20248bc4d43ee0629c5fddd2c88719
Kyuwoong Lee <kwooler@gmail.com>
2022-09-28 01:23:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 # ******************************
 # Make your Code
 # ******************************
+import random
+
+numbers1 = []
+numbers2 = []
+result = []
+for i in range(10):
+	numbers1.append(random.randint(0,100))
+	numbers2.append(random.randint(0,100))
+print (numbers1)
+print (numbers2)
